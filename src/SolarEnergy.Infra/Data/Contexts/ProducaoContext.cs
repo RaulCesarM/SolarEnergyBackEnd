2a85f0e4f6604b25e71d8827380006fc2b89fa2a
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-09-17 22:35:16 -0300

    Feature : Create Context

--- src/SolarEnergy.Infra/Data/Contexts/ProducaoContext.cs
+++ src/SolarEnergy.Infra/Data/Contexts/ProducaoContext.cs
@@ -1,10 +1,38 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SolarEnergy.Domain.Models;
+using SolarEnergy.Infra.Data.Mapping;
 
 namespace SolarEnergy.Infra.Data.Contexts
 {
-    public class ProducaoContext :DbContext
+    
+    public class ProducaoContext : DbContext
     {
-        
+        private readonly IConfiguration _configuration;
+        public ProducaoContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Producao> Producoes { get; set; }
+        public DbSet<Registro> Registros { get; set; }
+        public DbSet<Unidade> Unidades { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            base.OnConfiguring(options);
+            options.UseSqlServer(_configuration.GetConnectionString("DeveloperConection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProducaoMap());
+            modelBuilder.ApplyConfiguration(new RegistroMap());
+            modelBuilder.ApplyConfiguration(new UnidadeMap());
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+
+        }
     }
 }
